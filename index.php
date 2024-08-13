b3d2e55840c70c4a655aff1adc9cf1febca19ffd
AscendantSites <156096357+AscendantSites@users.noreply.github.com>
2024-08-13 19:38:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,43 @@
-Hiii
+<?php
+$domains = [
+    ["name" => "roblox.gt", "rating" => 5],
+    ["name" => "roblox.ml", "rating" => 4.5],
+];
+?>
+
+<div class="flex-1 p-4 md:p-8 pt-6 h-full">
+    <div class="mb-4 flex items-center space-x-1 text-sm text-muted-foreground">
+        <a href="/dashboard">Dashboard</a>
+        <svg width="15" height="15" viewBox="0 0 15 15" fill="none">
+            <path d="M6.1584 3.13508C6.35985 2.94621 6.67627 2.95642 6.86514 3.15788L10.6151 7.15788C10.7954 7.3502 10.7954 7.64949 10.6151 7.84182L6.86514 11.8418C6.67627 12.0433 6.35985 12.0535 6.1584 11.8646C5.95694 11.6757 5.94673 11.3593 6.1356 11.1579L9.565 7.49985L6.1356 3.84182C5.94673 3.64036 5.95694 3.32394 6.1584 3.13508Z" fill="currentColor"></path>
+        </svg>
+        <a class="font-medium text-foreground pointer-events-none" href="/dashboard/settings">Settings</a>
+    </div>
+    <div class="flex h-full w-full justify-center mt-16 items-center flex-col gap-y-12">
+        <h3 class="text-xl font-bold uppercase">Choose a domain</h3>
+        <div class="flex flex-col sm:flex-row items-center justify-center gap-x-12 gap-y-6">
+            <?php foreach ($domains as $domain): ?>
+            <div class="max-w-xs min-w-80 flex flex-col dark:bg-gradient border-t-4 shadow-sm rounded-xl border-emerald-500">
+                <div class="p-4 md:p-5">
+                    <h3 class="text-lg font-bold text-gray-800 dark:text-white"><?= $domain['name'] ?></h3>
+                    <div class="mt-2">
+                        <?php for ($i = 0; $i < 5; $i++): ?>
+                            <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeInherit <?= $i < $domain['rating'] ? 'MuiRating-iconFilled' : 'MuiRating-iconEmpty' ?>" focusable="false" aria-hidden="true" viewBox="0 0 24 24">
+                                <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                            </svg>
+                        <?php endfor; ?>
+                    </div>
+                    <div class="flex w-full justify-between mt-3">
+                        <a class="inline-flex items-center gap-x-1 text-sm font-semibold text-blue-600 hover:text-blue-800 cursor-pointer">Use domain
+                            <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2">
+                                <path d="M5 12h14"></path>
+                                <path d="m12 5 7 7-7 7"></path>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</div>
